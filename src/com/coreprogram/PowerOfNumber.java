d8c73e6292a808bf8ea8d07914845e307aae428a
jayashri23 <jsjankar23@gmail.com>
2022-12-03 22:19:21 +0530

    Calculating the Power Of Two

---
+++ src/com/coreprogram/PowerOfNumber.java
@@ -0,0 +1,22 @@
+package com.coreprogram;
+import java.util.Scanner;
+
+public class PowerOfNumber {
+    public static void Power(int num){
+        int a=1, b=(int) Math.pow(2,num);
+        if(num >=0 && num < 31) {
+            while (a < b) {
+                a = a * 2;
+                System.out.println(a);
+            }
+        }else
+            System.out.println("Invalid Number :");
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter the Power Value : ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println("\n The Power table of 2 : ");
+        Power(num);
+    }
+}
